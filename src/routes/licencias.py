f7f8c1d7c447038ec709e808611081c3329aaa1b
andresdelgadoc98@gmail.com <andresdelgadoc98@gmail.com>
2025-03-12 13:59:09 -0600

    se corregio error de put licencias

--- src/routes/licencias.py
+++ src/routes/licencias.py
@@ -2,7 +2,7 @@ from flask import request, jsonify, Blueprint
 from db.db import Licencia,db
 from flask_cors import cross_origin
 from datetime import datetime
-
+from uuid import UUID
 
 main = Blueprint('licencias', __name__)
 
@@ -19,15 +19,6 @@ def crear_licencia():
     db.session.commit()
     return jsonify({"mensaje": "Licencia creada exitosamente", "id": nueva_licencia.id}), 201
 
-@main.route("<string:id>", methods=["PUT"])
-def editar_licencia(id):
-    data = request.get_json()
-    licencia = Licencia.query.get_or_404(id)
-    licencia.nombre_licencia = data.get("nombre_licencia", licencia.nombre_licencia)
-    licencia.fecha_inicio = datetime.strptime(data.get("fecha_inicio"), "%Y-%m-%d") if data.get("fecha_inicio") else licencia.fecha_inicio
-    licencia.fecha_fin = datetime.strptime(data.get("fecha_fin"), "%Y-%m-%d") if data.get("fecha_fin") else licencia.fecha_fin
-    db.session.commit()
-    return jsonify({"mensaje": "Licencia actualizada exitosamente"}), 200
 
 @main.route("<string:id>", methods=["GET"])
 def obtener_licencia(id):
@@ -51,3 +42,39 @@ def obtener_licencias():
         "fecha_fin": licencia.fecha_fin.strftime("%Y-%m-%d"),
         "fecha_creacion": licencia.fecha_creacion.strftime("%Y-%m-%d %H:%M:%S"),
     } for licencia in licencias]), 200
+
+
+@main.route('<string:id>', methods=['PUT'])
+def editar_licencia(id):
+    try:
+        id_licencia = UUID(id)
+    except ValueError:
+        return jsonify({"error": "ID de licencia no válido"}), 400
+
+    licencia = Licencia.query.get(id_licencia)
+    if not licencia:
+        return jsonify({"error": "Licencia no encontrada"}), 404
+
+    datos = request.get_json()
+    if not datos:
+        return jsonify({"error": "Datos no proporcionados"}), 400
+
+    if "nombreLicencia" in datos:
+        licencia.nombre_licencia = datos["nombreLicencia"]
+    if "fechaInicio" in datos:
+        licencia.fecha_inicio = datetime.strptime(datos["fechaInicio"], "%Y-%m-%d").date()
+    if "fechaFin" in datos:
+        licencia.fecha_fin = datetime.strptime(datos["fechaFin"], "%Y-%m-%d").date()
+    if "estado" in datos:
+        licencia.estado = datos["estado"]
+
+    db.session.commit()
+
+    return jsonify({
+        "id": licencia.id,
+        "estado":  licencia.estado,
+        "nombre_licencia": licencia.nombre_licencia,
+        "fecha_inicio": licencia.fecha_inicio.isoformat(),
+        "fecha_fin": licencia.fecha_fin.isoformat(),
+        "fecha_creacion": licencia.fecha_creacion.isoformat()
+    }), 200
